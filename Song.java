8d5ef0a21b1fbbf10b9982b5d672203b0d289fc0
williammunnich <williammunnich@gmail.com>
2018-09-27 12:46:32 -0500

    Initial commit

---
+++ Song.java
@@ -0,0 +1,22 @@
+
+public class Song
+{
+    private int rating;
+    private String title;
+    private Float price;
+    
+    public Song()
+    {
+        rating = 0;
+        title = "";
+        price = 0.0;
+    }
+    public void setTitle(String t) {
+        title =t;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public Float 
+
+}
