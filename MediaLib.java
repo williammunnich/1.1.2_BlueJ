41dbd7b8e4cad080b76df74a5f98bfe77bd8c251
williammunnich <williammunnich@gmail.com>
2018-10-02 13:10:34 -0500

    attempting to work out a list

--- MediaLib.java
+++ MediaLib.java
@@ -1,27 +1,28 @@
 
 public class MediaLib
 {
-    private double totalCost;
+    public double totalCost;
     public int numSongs;
-    public  double testVal = 0.0;
-    
+    public static double testVal = 0.0;
+    public ArrayList totalRatings = ();
     public static void main()
     {
      System.out.println("\f");
      System.out.println("Welcome to your Media Libray");
      double totalCost = 0.0;
      int numSongs = 0;
+     List a = newArrayList();
      //Song song = new Song();
      Book book = new Book();
      Movie movie = new Movie();
      //System.out.println(song);
      //System.out.println(book);
      //System.out.println(movie);
-     Song song = new Song("yellow", 3.30, 4, true );
+     Song song = new Song("yellow", 3.30, 4, false );
      numSongs += 1;
-     Song song2 = new Song("blue", 3.30, 4, true );
+     Song song2 = new Song("blue", 3.30, 6, true );
      numSongs += 1;
-     Song song3 = new Song("green", 3.30, 4, true );
+     Song song3 = new Song("green", 3.30, 9, true );
      numSongs += 1;
      book.setTitle("Hunger games");
      movie.setTitle("Mr.Nobody");
@@ -34,9 +35,10 @@ public class MediaLib
      totalCost += song2.getPrice();
      totalCost += song3.getPrice();
      System.out.println(totalCost);
-     testVal = testVal + totalCost;
+     testVal += totalCost;
      testVal = testVal / numSongs;
-     
+     System.out.println(testVal);
+     totalRatings.append(song.getRating());
     }
 }
 
